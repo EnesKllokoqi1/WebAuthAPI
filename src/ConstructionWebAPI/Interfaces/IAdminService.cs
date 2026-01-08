c6ba5c607a957ff73edf3c30476b7e9bb178a32f
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-01-08 22:47:36 +0100

    feat: implement admin controller

---
+++ src/ConstructionWebAPI/Interfaces/IAdminService.cs
@@ -0,0 +1,14 @@
+﻿using ConstructionWebAPI.DTOS;
+using ConstructionWebAPI.Entities;
+
+namespace ConstructionWebAPI.Interfaces
+{
+    public interface IAdminService
+    {
+        Task<List<Worker>> GetAllWorkers();
+        Task<Worker?> GetWorkerDataById(Guid guid);
+        Task<bool> DeleteWorker(Guid guid);
+        Task<Worker?> CreateWorker(WorkerDTO createWorkerDTO);
+        Task<Worker?> UpdateWorkerData(Guid guid, WorkerDTO workerDTO);
+    }
+}
